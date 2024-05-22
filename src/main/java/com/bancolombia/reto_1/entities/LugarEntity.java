390596e28f0cfe7492fa13749fa4ed6cafe4665d
rigoberto <rigoberto.miranda1503@gmail.com>
2024-05-22 12:37:41 -0500

    se modifico las entidades para que los valores a ingresar no puedan ser nulos

--- src/main/java/com/bancolombia/reto_1/entities/LugarEntity.java
+++ src/main/java/com/bancolombia/reto_1/entities/LugarEntity.java
@@ -1,5 +1,6 @@
 package com.bancolombia.reto_1.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
@@ -20,8 +21,11 @@ public class LugarEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idLugar;
+     @Column(length = 40, nullable = false)
     private String nombre;
+    @Column(length = 40, nullable = false)
     private String departamento;
+    @Column(length = 40, nullable = false)
     private String pais;
 
     @ManyToOne(fetch = FetchType.LAZY)
